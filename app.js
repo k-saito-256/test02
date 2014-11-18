2ef495d721c4d9f9c5e95abce6390619cb4ad195
ksaito256 <k.saito.256@gmail.com>
2014-11-18 15:01:05 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){    
+    res.render('hello.ejs');   
+});
+
+var server = app.listen(process.env.PORT || 3000, 
+                        process.env.IP || "0.0.0.0", function () {
+        var host = server.address().address
+        var port = server.address().port
+        console.log('Example app listening at http://%s:%s', host, port)
+});
