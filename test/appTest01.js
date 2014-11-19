40faa21585c10e87ccceff427be86c075f0160d4
ksaito256 <k.saito.256@gmail.com>
2014-11-19 04:55:05 +0000

    mocha

--- test/appTest01.js
+++ test/appTest01.js
@@ -1,18 +1,16 @@
 var assert = require("assert")
+describe('app server test', function(){
+  it('app todo', function () {
+  	assert.ok( 1=='1')
+  });
+});
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5))
-      assert.equal(-1, [1,2,3].indexOf(0))
-    })
-  })
-})
-describe('Array2', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5))
-      assert.equal(-1, [1,2,3].indexOf(0))
-    })
-  })
+
+describe('Array', function () {
+	describe('#indexOf()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal(-1, [1, 2, 3].indexOf(5))
+			assert.equal(-1, [1, 2, 3].indexOf(0))
+		})
+	})
 })
