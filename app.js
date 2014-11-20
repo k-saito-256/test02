4ae56f7f1cd7bb0a97e9b998fc1f7e817be4ab8d
ksaito256 <k.saito.256@gmail.com>
2014-11-20 09:21:13 +0000

    public added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 
+
 var app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function(req, res){    
     res.render('hello.ejs');   
@@ -13,3 +15,6 @@ var server = app.listen(process.env.PORT || 3000,
         var port = server.address().port
         console.log('Example app listening at http://%s:%s', host, port)
 });
+
+
+
